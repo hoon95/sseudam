feaf20e6d1ee9566f49d73345f570d66768ec0b6
hoon95 <66771509+hoon95@users.noreply.github.com>
2024-12-11 23:22:28 +0900

    🔥 Project init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { ApolloProvider } from '@apollo/client'
-import { apolloClient } from './lib/apollo'
-import { ItemList } from './components/ItemList'
-
 export const App = () => {
   return (
-    <ApolloProvider client={apolloClient}>
-      <ItemList />
-    </ApolloProvider>
+    <>
+      
+    </>
   )
 }
--- src/lib/apollo.tsx
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
-
-export const apolloClient = new ApolloClient({
-  uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
-  cache: new InMemoryCache(),
-  headers: {
-    apikey: import.meta.env.SUPABASE_API_KEY,
-  },
-}) 
