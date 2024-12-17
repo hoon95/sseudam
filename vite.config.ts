53de8bbf30b61dd67531ab6f75927a71991cf65f
hoon95 <maruon95@gmail.com>
2024-12-17 18:19:56 +0900

    🔧 vite script, package 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-// https://vite.dev/config/
+const getAliasPath = (dir: string) => path.resolve(__dirname, dir);
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": getAliasPath("./src"),
+      "@assets": getAliasPath("./src/assets"),
+      "@components": getAliasPath("./src/components"),
+      "@pages": getAliasPath("./src/pages"),
+      "@store": getAliasPath("./src/store"),
+      "@apis": getAliasPath("./src/apis"),
+    },
+  },
+});
