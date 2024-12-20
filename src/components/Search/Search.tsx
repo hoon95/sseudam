b4e53c107249c4937a160b87d64fdb852bba1931
hoon95 <maruon95@gmail.com>
2024-12-20 15:20:07 +0900

    👷 CI Build Test & Dockerfile

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -117,28 +117,28 @@ export const Search = () => {
               <img src={item.filename} alt="유기동물 사진" />
               <CardContent>
                 <Typography className="text">
+                  <p>
+                    공고날짜: {item.notice_sdt} - {item.notice_edt}
+                  </p>
+                  <p>품종: {item.kind_cd}</p>
+                  <p>나이: {item.age}</p>
+                  <p>성별: {item.sex_cd}</p>
                   {/* <p>유기번호: {item.desertion_no}</p> */}
                   {/* <p>접수일: {item.happen_dt}</p> */}
                   {/* <p>발견장소: {item.happen_place}</p> */}
-                  <p>품종: {item.kind_cd}</p>
                   {/* <p>색상: {item.color_cd}</p> */}
-                  <p>나이: {item.age}</p>
                   {/* <p>체중: {item.weight}</p> */}
                   {/* <p>공고번호: {item.notice_no}</p> */}
-                  {/* <p>
-                공고날짜: {item.notice_sdt} - {item.notice_edt}
-              </p> */}
                   {/* <p>상태: {item.process_state}</p> */}
-                  <p>성별: {item.sex_cd}</p>
                   {/* <p>중성화여부: {item.neuter_yn}</p> */}
-                  <p>특징: {item.special_mark}</p>
+                  {/* <p>특징: {item.special_mark}</p> */}
                   {/* <p>보호소이름: {item.care_nm}</p> */}
                   {/* <p>보호소 전화번호: {item.care_tel}</p> */}
                   {/* <p>보호장소: {item.care_addr}</p> */}
                   {/* <p>관할기관: {item.org_nm}</p> */}
                   {/* <p>담당자: {item.charge_nm}</p> */}
                   {/* <p>담당자 연락처: {item.officetel}</p> */}
-                  <p>특이사항: {item.notice_comment}</p>
+                  {/* <p>특이사항: {item.notice_comment}</p> */}
                 </Typography>
               </CardContent>
               <CardContent>
