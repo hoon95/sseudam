1b9110a110c5b24375e76367dcfbab3f710376fe
hoon95 <maruon95@gmail.com>
2024-12-31 18:33:00 +0900

    ✨ Detail - vis.gl/react-google-maps 연동

--- src/components/PetList/PetDetail/PetDetail.styled.tsx
+++ src/components/PetList/PetDetail/PetDetail.styled.tsx
@@ -8,23 +8,105 @@ export const Detail = styled.div`
   margin: auto;
 
   img {
-    width: 50%;
+    width: 70%;
+    max-width: 70%;
+    max-height: 50vh;
+    border: 1px solid var(--dark);
+    border-radius: 10px;
+    margin-bottom: calc(var(--gap) * 2);
   }
 
   & .title {
-    width: 50%;
+    width: 70%;
     display: flex;
-    justify-content: start;
-    align-items: end;
-    font-size: var(--text-md);
-    padding-top: var(--gap);
+    justify-content: space-between;
+    font-size: calc(var(--text-md) * 0.7);
 
-    & .kind {
-      font-size: var(--text-lg);
+    & .desc {
+      display: flex;
+      align-items: center;
+
+      & .kind {
+        font-size: var(--text-lg);
+      }
+
+      & .sex {
+        padding-left: var(--gap);
+        padding-right: calc(var(--gap) * 0.5);
+      }
+    }
+  }
+
+  & .divider {
+    width: 70%;
+    margin: calc(var(--gap) * 1.5) 0;
+  }
+`;
+
+export const BtnContainer = styled.div`
+  display: flex;
+  gap: calc(var(--gap) * 0.5);
+
+  & .list {
+    color: var(--light);
+  }
+`;
+
+export const Info = styled.ul`
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  gap: var(--gap);
+
+  li {
+    display: flex;
+    align-items: center;
+    gap: var(--gap);
+
+    p {
+      :first-of-type {
+        width: 20%;
+        font-size: calc(var(--text-md) * 0.7);
+      }
     }
 
-    & .sex {
-      padding: 0 var(--gap);
+    &.careNm {
+      justify-content: space-between;
+
+      & .title {
+        width: 20%;
+      }
+
+      & .container {
+        width: 80%;
+        display: flex;
+        flex-direction: column;
+        gap: var(--gap);
+
+        & .text {
+          width: 100%;
+
+          p {
+            width: 100%;
+          }
+
+          & .call {
+            display: flex;
+            align-items: center;
+            gap: calc(var(--gap) * 0.2);
+            padding-top: calc(var(--gap) * 0.7);
+          }
+        }
+
+        & .map {
+          height: 50vh;
+
+          & .gmnoscreen,
+          .gmnoprint {
+            display: none;
+          }
+        }
+      }
     }
   }
 `;
--- src/components/PetList/PetDetail/PetDetail.tsx
+++ src/components/PetList/PetDetail/PetDetail.tsx
@@ -1,11 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { fetchDetail } from "@apis/supabase";
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+import { Button, Divider } from "@mui/material";
 import MaleIcon from "@mui/icons-material/Male";
 import FemaleIcon from "@mui/icons-material/Female";
+import CallIcon from "@mui/icons-material/Call";
 import { blue, pink } from "@mui/material/colors";
-import { Detail } from "./PetDetail.styled";
+import { Detail, BtnContainer, Info } from "./PetDetail.styled";
 
 export const PetDetail = () => {
   const { id } = useParams();
@@ -17,25 +22,130 @@ export const PetDetail = () => {
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error...</p>;
-
   console.log(data);
   const item = data[0];
+
+  interface LatLng {
+    lat: number | null;
+    lng: number | null;
+  }
+
+  const TooltipMap = () => {
+    // lat, lng 가져오기
+    const [latLng, setLatLng] = useState<LatLng | null>(null);
+
+    const addr = item.care_addr;
+    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+    const getLatLng = async (address: string) => {
+      const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+        address,
+      )}&key=${apiKey}`;
+
+      try {
+        const response = await axios.get(url);
+
+        if (response.data.status === "OK") {
+          const { lat, lng } = response.data.results[0].geometry.location;
+          setLatLng({ lat, lng });
+        }
+      } catch (error) {
+        console.error("지도 API 오류: ", error);
+      }
+    };
+
+    useEffect(() => {
+      getLatLng(addr);
+    }, []);
+
+    if (latLng) {
+      const lat = latLng?.lat ?? 0;
+      const lng = latLng?.lng ?? 0;
+
+      return (
+        <APIProvider apiKey={apiKey}>
+          <Map
+            className="map"
+            defaultCenter={{ lat: lat, lng: lng }}
+            defaultZoom={15}
+            gestureHandling={"greedy"}
+            disableDefaultUI={true}
+          >
+            <Marker position={{ lat: lat, lng: lng }} />
+          </Map>
+        </APIProvider>
+      );
+    }
+  };
+
   return (
     <Detail>
       <img src={item.popfile} alt="" />
+
       <div className="title">
-        <p className="kind">{item.kind}</p>
-        <p className="sex">
-          {item.sex_cd === "M" ? (
-            <MaleIcon sx={{ color: blue[700] }} />
-          ) : (
-            <FemaleIcon sx={{ color: pink[700] }} />
-          )}
-        </p>
-        <p className="age">{item.calculated_age}세</p>
-        <p>&nbsp;/&nbsp;</p>
-        <p className="weight">{item.calculated_weight}kg</p>
+        <div className="desc">
+          <p className="kind">{item.kind}</p>
+          <p className="sex">
+            {item.sex_cd === "M" ? (
+              <MaleIcon sx={{ color: blue[700] }} />
+            ) : (
+              <FemaleIcon sx={{ color: pink[700] }} />
+            )}
+          </p>
+          <p className="age">{item.calculated_age}세</p>
+          <p>&nbsp;/&nbsp;</p>
+          <p className="weight">{item.calculated_weight}kg</p>
+        </div>
+        <BtnContainer>
+          <Button variant="contained" color="success">
+            문의하기
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            className="list"
+            onClick={() => history.back()}
+          >
+            목록
+          </Button>
+        </BtnContainer>
       </div>
+      <Divider className="divider" />
+      <Info>
+        <li>
+          <p>상태</p>
+          <p>보호중</p>
+        </li>
+        <li>
+          <p>중성화 여부</p>
+          <p>
+            {item.neuter_yn === "Y"
+              ? "O"
+              : item.neuter_yn === "N"
+                ? "X"
+                : "모름"}
+          </p>
+        </li>
+        <li>
+          <p>특이사항</p>
+          <p>{item.special_mark}</p>
+        </li>
+      </Info>
+      <Divider className="divider" />
+      <Info>
+        <li className="careNm">
+          <p className="title">보호장소</p>
+          <div className="container">
+            <div className="text">
+              <p>{item.care_nm}</p>
+              <div className="call">
+                <CallIcon />
+                <p>{item.officetel}</p>
+              </div>
+            </div>
+            <TooltipMap />
+          </div>
+        </li>
+      </Info>
     </Detail>
   );
 };
